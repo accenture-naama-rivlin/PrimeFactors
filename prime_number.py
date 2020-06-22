bc9b14c83c203a07a026ab4c243fd2254a5a606c
Naama Rivlin <naama.rivlin@accenture.com>
2020-06-22 11:27:16 -0400

    Three returns three

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,6 @@
 class PrimeNumber:
 
     def factorize(self, number):
+        if number > 2:
+            return [3]
         return [2]
--- test_prime_number.py
+++ test_prime_number.py
@@ -3,9 +3,13 @@ import unittest
 
 class TestPrimeNumber(unittest.TestCase):
 
+    number = prime_number.PrimeNumber()
+
     def test_two_returns_two(self):
-        number = prime_number.PrimeNumber()
-        self.assertEqual([2], number.factorize(2))
+        self.assertEqual([2], self.number.factorize(2))
+
+    def test_three_returns_three(self):
+        self.assertEqual([3], self.number.factorize((3)))
 
 if __name__ == "__main__":
     unittest.main()
